b27b1f1405ee913b7311eeea81646333c5e8d23e
Trent Schneider <trentonschneider@gmail.com>
2020-10-07 15:44:19 -0400

    adjusted label htmlFor to for in filterForm.js

--- employeedirectory/src/components/FilterForm/index.js
+++ employeedirectory/src/components/FilterForm/index.js
@@ -4,7 +4,7 @@ function FilterForm(props) {
   return (
     <form>
       <div className="form-group">
-        <label htmlFor="filter">Filter employees by first or last name:</label>
+        <label for="filter">Filter employees by first or last name:</label>
         <input
           onChange={props.handleInputChange}
           value={props.search}
